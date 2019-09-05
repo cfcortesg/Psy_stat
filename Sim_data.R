02d3f5a222b44d5ac846d8a3fe59c6e84f3a8e5c
cfcortesg <constanza.c19@gmail.com>
2019-09-04 20:40:47 -0400

    first idea on simulation of data with DIF

--- Sim_data.R
+++ Sim_data.R
@@ -1,2 +1,19 @@
 ##### Simulation data
 
+#In this simulation data we need:
+# n = obs
+# n_it = number of items
+# b = difficulty parameter in rasch model (grid)
+# n_dif = number of items with DIF
+# p = probability of being referenced or focal group
+
+#In this case the item with dif is the one where de difference in difficulty (b parameter in Rasch Model) 
+#between the refence and focal group is random()
+#We asume discrimination is the same (Rasch) and the two groups, balanced or unbalanced, have the same hability distribution 
+
+sim_data = function(n, b){
+  
+  
+  
+  
+}
